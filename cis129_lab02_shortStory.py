f816c5fc7bbc8b41ea346adb8e4ae27d79ddc49a
isaacxp <47707970+isaacxp@users.noreply.github.com>
2025-02-19 14:22:55 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('hello world')
+print('The quick brown fox jumped over the lazy dog')
+print('The cranky farmer yelled at the noisy rooster')
+
+# Author: Isaac Cunningham
+# My first Python lab in my CIS129 course
+# Prints a simple short story
