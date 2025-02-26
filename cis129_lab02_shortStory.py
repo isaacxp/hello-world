772df7c759423cdfca7e2f598e9ee6a725dee675
isaacxp <47707970+isaacxp@users.noreply.github.com>
2025-02-26 14:38:49 -0700

    Delete cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++
@@ -1,6 +0,0 @@
-print('The quick brown fox jumped over the lazy dog')
-print('The cranky farmer yelled at the noisy rooster')
-
-# Author: Isaac Cunningham
-# My first Python lab in my CIS129 course
-# Prints a simple short story
